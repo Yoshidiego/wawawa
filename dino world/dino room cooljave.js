bdfc05c5fef5375b7fa460df1dcea226cd6f1e68
Yoshidiego <146372389+Yoshidiego@users.noreply.github.com>
2023-10-18 10:52:07 -0600

    Add files via upload

--- dino world/dino room cooljave.js
+++ dino world/dino room cooljave.js
@@ -1,50 +1,53 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+var firebaseConfig ={
+// Import the functions you need f rom the SDKs you need
+
+import { initializeApp } from ,"firebase/app" ;
+import { getAnalytics } from ,"firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBQlI8Ci4u95MoCXqdfIiwhWcH7GQiKPrI",
-  authDomain: "dinobase-522b3.firebaseapp.com",
-  projectId: "dinobase-522b3",
-  storageBucket: "dinobase-522b3.appspot.com",
-  messagingSenderId: "638860434133",
-  appId: "1:638860434133:web:911883ed7cb1cdb984a05c",
-  measurementId: "G-0X4G738YSS"
+const :firebaseConfig = {
+  apiKey: "AIzaSyA3mCPiAtZUCprDycKzj7Je52rqfNktmxg",
+  authDomain: "dino-24d73.firebaseapp.com",
+  databaseURL: "https://dino-24d73-default-rtdb.firebaseio.com",
+  projectId: "dino-24d73",
+  storageBucket: "dino-24d73.appspot.com",
+  messagingSenderId: "319051902060",
+  appId: "1:319051902060:web:aba05f5d27803253c2fc49",
+  measurementId: "G-S9HEQE3D2J"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig),;
 const analytics = getAnalytics(app);
-  
+}
   // Initialize Firebase
  // cambia "const app" por "firebase."
  firebase.initializeApp(firebaseConfig);
+user_name= localStorage.getItem("user_name");
+Room_names = localStorage.getItem("Room_names");
+document.getElementById("user_name").innerHTML = "¡Hola" +user_name+"¡";
 
 
 
 
-function getData() {firebase.database().ref("https://dinobase-522b3-default-rtdb.firebaseio.com/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Inicio del código
-
-    //Final del código
-    });});}
-getData();
 
 
 function addRoom()
 {
   Room_names = document.getElementById("Room_names").value;
-  firebase.database().ref("https://dinobase-522b3-default-rtdb.firebaseio.com/").child(Room_names).update({purpose :"adding Room_names"});
+  firebase.database().ref("https://dino-24d73-default-rtdb.firebaseio.com/").child(Room_names).update({purpose :"adding Room_names"});
   localStorage.setItem("Room_names" = Room_names);
   windows.location="dino room.html";
-}
 
+
+localStorage.setItem("Room_names",Room_names);
+windows.location.replace("hyml.html");
+}
 function redirectToRoomName(name)
+
 {
   console.log(name);
   localStorage.setItem("Room_names",name);
@@ -69,3 +72,18 @@ function send()
   });
   document.getElementById("msg").value="";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
